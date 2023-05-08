3cce79ed1378a1dc282484d223c028c83fde9abc
mehara00 <119718801+MeharaSahabandu@users.noreply.github.com>
2023-05-08 14:31:30 +0530

    ex:1 completed (Greetin component)

--- afProject/src/App.jsx
+++ afProject/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Greeting from "./components/Greeting";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>Hello World</div>
-  )
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/greeting" element={<Greeting />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ afProject/src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Greeting() {
+  return (
+    <div>
+      <h3>Hello World !!</h3>
+    </div>
+  );
+}
